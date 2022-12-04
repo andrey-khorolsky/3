0ef0a19a53ae51f4f7ba1187c2b2a74913d6f487
andrew <andreyhorolsky@gmail.com>
2022-12-04 18:59:19 +0300

    add carousel

--- scripts/firstscript.js
+++ scripts/firstscript.js
@@ -1,42 +1,43 @@
-function carrs(){
 
-     var fotos = [
-        "img/nsx.jpg",
-        "img/skyliner32.jpg",
-        "img/s2k.jpg",
-        "img/prelude.jpg",
-        "img/skyline32.jpg",
-        "img/por.jpg",
-        "img/por911.jpg",
-        "img/rx7.jpg",
-        "img/miata.jpg",
-        "img/miata2.jpg",
-        "img/datsun.jpg",
-        "img/fairl.jpg",
-        "img/180sx.jpg",
-        "img/180sx2.jpg",
-        "img/180sx3.jpg"
-    ];
 
-    var titles = [
-        "Honda NSX",
-        "Nissan Skyline GT-R R32",
-        "Honda S2000",
-        "Honda Prelude",
-        "Nissan Skyline GT-R R32",
-        "Porsche 911",
-        "Porsche 911 RAUH-Welt Begriff",
-        "Mazda RX-7 FD RocketBunny",
-        "Mazda Mx-5 Miata",
-        "Mazda Mx-5 Miata",
-        "Datsun 620",
-        "Nissan 240Z Fairlady",
-        "Nissan 180SX",
-        "Nissan 180SX",
-        "Nissan 180SX"
-    ];
+    var fotos = [
+    "img/nsx.jpg",
+    "img/skyliner32.jpg",
+    "img/s2k.jpg",
+    "img/prelude.jpg",
+    "img/skyline32.jpg",
+    "img/por.jpg",
+    "img/por911.jpg",
+    "img/rx7.jpg",
+    "img/miata.jpg",
+    "img/miata2.jpg",
+    "img/datsun.jpg",
+    "img/fairl.jpg",
+    "img/180sx.jpg",
+    "img/180sx2.jpg",
+    "img/180sx3.jpg"
+];
 
-        
+var titles = [
+    "Honda NSX",
+    "Nissan Skyline GT-R R32",
+    "Honda S2000",
+    "Honda Prelude",
+    "Nissan Skyline GT-R R32",
+    "Porsche 911",
+    "Porsche 911 RAUH-Welt Begriff",
+    "Mazda RX-7 FD RocketBunny",
+    "Mazda Mx-5 Miata",
+    "Mazda Mx-5 Miata",
+    "Datsun 620",
+    "Nissan 240Z Fairlady",
+    "Nissan 180SX",
+    "Nissan 180SX",
+    "Nissan 180SX"
+];
+
+function carrs(){ 
+    
     var all = $("<div></div>");
         for (var i=0; i<fotos.length; i++){
             var s = $("<div class='phtalb'></div>");
@@ -65,13 +66,13 @@ function carrs(){
                 });
                 
                 lsaap.click(function(){
-                    console.log(openimg.src);
-                    console.log(titles.indexOf(openimg.alt));
+                    updCarrs(-1);
+                    // console.log(titles.indexOf(openimg.alt));
                     openimg.src = fotos[Number(titles.indexOf(openimg.alt)) - Number(1)];
                 });
                 
                 rsaap.click(function(){
-                    console.log("right");
+                    updCarrs(1);
                 });
                 
                 d.append(arrows);
@@ -84,5 +85,16 @@ function carrs(){
     all.addClass('phtalb_d');
     $("body").append(all);
 };
+
+function updCarrs(inex){
+    let neme = String($(" .openedPhoto > img").attr("src"));
+    console.log(neme.substring(neme.indexOf("img/")));
+    console.log(fotos.indexOf(neme.substring(neme.indexOf("img/"))));
+    let curI = Number(fotos.indexOf(neme.substring(neme.indexOf("img/"))));
+    if (curI+inex == fotos.length) curI = -1;
+    if (curI+inex == -1) curI = fotos.length;
+
+    $(" .openedPhoto > img").attr("src", fotos[curI+inex]);
+}
 
 window.onload = carrs();
