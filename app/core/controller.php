61125410d76137ef32d5d45614704631a6cceaec
andrew <andreyhorolsky@gmail.com>
2023-03-25 19:59:14 +0300

    upd core controller

--- app/core/controller.php
+++ app/core/controller.php
@@ -1,8 +1,12 @@
 <?php
-
+require_once("app/core/view.php");
 class Controller{
     
     public $model;
     public $view;
+
+    function __construct(){
+        $this->view = new View();
+    }
 
 }
