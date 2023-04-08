c9a910704784d9330fa4248d91cc71eef209bf5d
andrew <andreyhorolsky@gmail.com>
2023-04-08 17:13:04 +0300

    basic ar

--- app/core/basicActiveRecord.php
+++ app/core/basicActiveRecord.php
@@ -2,15 +2,16 @@
 
 class BasicActiveRecord{
 
-    private static $database;
+    private static $database = "web-my-site";
+    protected static $table;
     public static $pdo;
-    private $fields = [];
+    private $id;
 
     function __construct(){
         $this->createConnect();
     }
 
-    function createConnect(){
+    static function createConnect(){
         try{
         static::$pdo = new PDO("mysql:host=localhost; dbname=web-my-site", "root", "123");
         } catch (PDOException $e){
@@ -21,17 +22,50 @@ class BasicActiveRecord{
 
     function save(){
 
+
     }
 
     function delete(){
+        static::$pdo->query("DELETE FROM `".static::$table."` WHERE id = ".$this->id);
 
     }
 
-    function find($id){
+    static function find($id){
+        static::createConnect();
+        $res = static::$pdo->query("SELECT * FROM `".static::$table."` WHERE id = $id");
+        $res = $res->fetch();
+
+        if (!$res) return null;
+
+        $obj = new static();
+        foreach ($res as $key => $value) {
+            $obj ->$key = $value;
+        }
+        return $obj;
 
     }
 
-    function FindAll(){
+    static function FindAll(){
+        static::createConnect();
+        
+        $objs = [];
+        
+        try{
+            // echo static::$table." - ";
+            $res = static::$pdo->query("SELECT * FROM `".static::$table."`");
+
+            while ($val = $res->fetch()){
+                $obj = new static();
+                foreach ($val as $key=>$value)
+                    $obj->$key = $value;
+                $objs[] = $obj;
+            }
+
+        } catch (PDOException $ex){
+            echo "trouble ".$ex." end\n";
+        }
+       
+        return $objs;
 
     }
 
