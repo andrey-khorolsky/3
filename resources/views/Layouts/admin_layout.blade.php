9bd112aaa84e5eba529276afc7eb4f6dc283c70e
andrew <horolskyandrey@gmail.com>
2023-05-23 19:12:16 +0300

    admin stuff

---
+++ resources/views/Layouts/admin_layout.blade.php
@@ -0,0 +1,41 @@
+<!DOCTYPE html>
+<html lang="ru">
+
+    <head>
+        <meta charset="utf-8">
+        <link rel="stylesheet" type="text/css" href="{{ asset('/css/style.css') }}">
+        <link type="image/x-icon" href="/public/img/hotdog.ico" rel="icon">
+        <script src="/public/js/jquery-3.6.1.js"></script>
+        <script src="/public/js/goods.js"></script>
+
+        <title>Администратор</title>
+        <link rel="stylesheet" type="text/css" href="/css/for_admin.css">
+
+        @yield('head')
+    </head>
+
+    <body>
+        <nav class="nav">
+            <ul>
+                <li> <a href="/"><img src="">Главная</a></li>
+                <li> <a href="/guest"><img src="">Гостевая книга</a> </li>
+                <li> <a onclick="openMenu();"><img src="">Мои интересы</a></li>
+                <li> <a href="/photoalbum"><img src="">Фото</a></li>
+                <li> <a href="/contacts"><img src="">Контакты</a></li>
+                <li> <a href="/test"><img src="">Тест</a></li>
+                <li> <a href="/account"><img src="">Аккаунт</a></li>
+            </ul>
+        </nav>
+
+        <div class="admpanel">
+            <a href="/guest/addCommentsFromFile">Добавить комментарии из файла</a>
+            <a href="/blog/newArticle">Добавить запись в блог</a>
+            <a href="/blog/addFileWithArticles">Добавить запись в блог из файла</a>
+            <a href="admin/statistic">Статистика посещений</a>
+        </div>
+
+        @yield("content")
+
+    </body>
+    
+</html>
